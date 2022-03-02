a2a901c0a68336f167b526d1c6da96fee111608f
Charles An <mucollabo@gmail.com>
2022-03-02 23:37:47 +0900

    until chap 9_4

---
+++ chapter09/code_09_02.playground/Contents.swift
@@ -0,0 +1,12 @@
+struct BasicInformation {
+    var name: String
+    var age: Int
+}
+
+var charlesInfo: BasicInformation = BasicInformation(name: "charles", age: 99)
+charlesInfo.age = 100
+charlesInfo.name = "Merhaba"
+
+let merhabaInfo: BasicInformation = BasicInformation(name: "Merhaba", age: 99)
+merhabaInfo.age = 100
+jennyInfo.age = 100
---
+++ chapter09/code_09_04.playground/Contents.swift
@@ -0,0 +1,12 @@
+class Person {
+    var height: Float = 0.0
+    var weight: Float = 0.0
+}
+
+var charles: Person = Person()
+charles.height = 123.4
+charles.weight = 123.4
+
+let jenny: Person = Person()
+jenny.height = 123.4
+jenny.weight = 123.4
